e57a88fcefc892b0d7ef20cac79d922dba8e0b38
nikitastryuk <nikita.stryuk@wonderslab.com>
2019-01-08 00:59:19 +0200

    lint-staged test

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = 2;
+function test(name) {
+    console.log(name);
+}
+console.log(123, 1, 2);
+test('sdas');
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Test
+var a: any = 2;
+function test(name: any): void {
+  console.log(name);
+}
+console.log(123, 1, 2)
+test('sdas')
+      console.log(1)
+       console.log(1)
