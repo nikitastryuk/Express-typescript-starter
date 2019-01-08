37bca573c5ac251ba3e5dcb1070faf4e9e404c6f
nikitastryuk <nikita.stryuk@wonderslab.com>
2019-01-08 02:09:40 +0200

    test5

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 // Test
-var a: any = 2;
+let a: any = 2;
 function test(name: any): void {
   console.log(name);
 }
-console.log(123, 1, 2)
-test('sdas')
-      console.log(1)
-       console.log(1)
+console.log(123, 1, 2);
+test('sdas');
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
